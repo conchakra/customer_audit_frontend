162ba84778cc9e2bf6c0ec09d6822f0f78ef78e4
Sinchana <tssinchana@gmail.com>
2026-05-22 10:18:12 +0530

    Added customer API integration and pagination

--- screens/CustomerScreen.tsx
+++ screens/CustomerScreen.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import API_BASE_URL from '../src/config/api';
+import React, { useState, useEffect } from 'react';
 
 import {
   View,
@@ -8,50 +9,124 @@ import {
   FlatList,
   Image,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
 
-import { Ionicons } from '@expo/vector-icons';
 
-const customers = [
-  {
-    id: '1',
-    name: 'Rakesh Verma',
-    phone: '9876543210',
-    balance: '₹12,500',
-    image: 'https://i.pravatar.cc/100?img=1',
-  },
-  {
-    id: '2',
-    name: 'Arjun Patel',
-    phone: '9123456780',
-    balance: '₹8,200',
-    image: 'https://i.pravatar.cc/100?img=2',
-  },
-  {
-    id: '3',
-    name: 'Rahul Sharma',
-    phone: '9988776655',
-    balance: '₹15,780',
-    image: 'https://i.pravatar.cc/100?img=3',
-  },
-  {
-    id: '4',
-    name: 'Kiran Kumar',
-    phone: '9011223344',
-    balance: '₹4,500',
-    image: 'https://i.pravatar.cc/100?img=4',
-  },
-];
+import { Ionicons } from '@expo/vector-icons';
 
 export default function CustomerScreen() {
 
   const [search, setSearch] = useState('');
+  const [customers, setCustomers] = useState<any[]>([]);
+  const [page, setPage] = useState(1);
+const [loadingMore, setLoadingMore] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const filteredCustomers = customers.filter(
-    (item) =>
-      item.name.toLowerCase().includes(search.toLowerCase()) ||
-      item.phone.includes(search)
+const fetchCustomers = (pageNumber=1) => {
+
+  fetch(`${API_BASE_URL}/customer/api/v1/customers?page=${pageNumber}&size=5`)
+    .then((response) => response.json())
+    .then((data) => {
+
+      console.log('API DATA:', data);
+      if (pageNumber === 1) {
+  setCustomers(data);
+} else {
+  setCustomers((prev) => [...prev, ...data]);
+}
+      setLoading(false);
+      setRefreshing(false);
+
+    })
+    .catch((error) => {
+
+      console.log(error);
+
+      setLoading(false);
+      setRefreshing(false);
+
+    });
+
+};
+
+useEffect(() => {
+  fetchCustomers();
+}, []);
+
+const onRefresh = () => {
+
+  setRefreshing(true);
+
+  fetchCustomers();
+
+};
+
+const loadMoreCustomers = () => {
+
+  if (loadingMore) return;
+
+  setLoadingMore(true);
+
+  const nextPage = page + 1;
+
+  fetch(
+    `${API_BASE_URL}/customer/api/v1/customers?page=${nextPage}&size=5`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+
+      if (data.length > 0) {
+
+        setCustomers((prev) => [...prev, ...data]);
+
+        setPage(nextPage);
+
+      }
+
+      setLoadingMore(false);
+
+    })
+    .catch((error) => {
+
+      console.log(error);
+
+      setLoadingMore(false);
+
+    });
+
+};
+
+if (loading) {
+
+  return (
+
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+
+      <ActivityIndicator
+        size="large"
+        color="#001B44"
+      />
+
+    </View>
+
   );
+}
+
+const filteredCustomers = customers.filter(
+  (item) =>
+    item.name
+      ?.toLowerCase()
+      .includes(search.toLowerCase()) ||
+    item.phone?.includes(search)
+);
 
   return (
 
@@ -63,35 +138,53 @@ export default function CustomerScreen() {
 
       <View style={styles.searchContainer}>
 
-  <Ionicons
-    name="search"
-    size={20}
-    color="gray"
-    style={styles.searchIcon}
-  />
+        <Ionicons
+          name="search"
+          size={20}
+          color="gray"
+          style={styles.searchIcon}
+        />
 
-  <TextInput
-    placeholder="Search by name or phone"
-    value={search}
-    onChangeText={setSearch}
-    style={styles.searchBar}
-  />
+        <TextInput
+          placeholder="Search by name or phone"
+          value={search}
+          onChangeText={setSearch}
+          style={styles.searchBar}
+        />
 
-</View>
+      </View>
+
+{loading ? (
+  <ActivityIndicator size="large" color="blue" />
+) : (
 
       <FlatList
         data={filteredCustomers}
-        keyExtractor={(item) => item.id}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+        onEndReached={loadMoreCustomers}
+        onEndReachedThreshold={0.5}
+        keyExtractor={(item,index) => index.toString()}
         showsVerticalScrollIndicator={false}
+        ListFooterComponent={
+  loadingMore ? (
+    <ActivityIndicator
+      size="small"
+      color="#001B44"
+      style={{ marginVertical: 20 }}
+    />
+  ) : null
+}
         renderItem={({ item }) => (
 
+
+          
           <TouchableOpacity style={styles.card}>
 
-            {/* LEFT */}
             <View style={styles.leftSection}>
 
               <Image
-                source={{ uri: item.image }}
+                source={{ uri:'https://i.pravatar.cc/100 ' }}
                 style={styles.avatar}
               />
 
@@ -109,12 +202,7 @@ export default function CustomerScreen() {
 
             </View>
 
-            {/* RIGHT */}
             <View style={styles.rightSection}>
-
-              <Text style={styles.balance}>
-                {item.balance}
-              </Text>
 
               <Ionicons
                 name="chevron-forward"
@@ -128,6 +216,7 @@ export default function CustomerScreen() {
 
         )}
       />
+)}
 
     </View>
   );
@@ -149,23 +238,23 @@ const styles = StyleSheet.create({
   },
 
   searchContainer: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  backgroundColor: '#f3f4f6',
-  borderRadius: 14,
-  paddingHorizontal: 15,
-  marginBottom: 20,
-},
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f3f4f6',
+    borderRadius: 14,
+    paddingHorizontal: 15,
+    marginBottom: 20,
+  },
 
-searchIcon: {
-  marginRight: 10,
-},
+  searchIcon: {
+    marginRight: 10,
+  },
 
- searchBar: {
-  flex: 1,
-  paddingVertical: 15,
-  fontSize: 16,
-},
+  searchBar: {
+    flex: 1,
+    paddingVertical: 15,
+    fontSize: 16,
+  },
 
   card: {
     flexDirection: 'row',
@@ -201,13 +290,6 @@ searchIcon: {
 
   rightSection: {
     alignItems: 'flex-end',
-  },
-
-  balance: {
-    fontSize: 14,
-    fontWeight: '500',
-    color:'gray',
-    marginBottom: 5,
   },
 
 });
